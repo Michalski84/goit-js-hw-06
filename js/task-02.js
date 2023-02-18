a9329183856d2e519c64fd019b6331b615abd6a5
Andrzej Michalski <113372492+Michalski84@users.noreply.github.com>
2023-02-18 22:24:18 +0100

    z2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+const listElement1 = document.createElement("li");
+listElement1.textContent = ingredients[0];
+const listElement2 = document.createElement("li");
+listElement2.textContent = ingredients[1];
+const listElement3 = document.createElement("li");
+listElement3.textContent = ingredients[2];
+const listElement4 = document.createElement("li");
+listElement4.textContent = ingredients[3];
+const listElement5 = document.createElement("li");
+listElement5.textContent = ingredients[4];
+const listElement6 = document.createElement("li");
+listElement6.textContent = ingredients[5];
+list.append(listElement1, listElement2, listElement3, listElement4, listElement5, listElement6 )
+console.log(list);
