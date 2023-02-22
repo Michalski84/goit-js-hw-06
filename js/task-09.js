5a5d5a00a83efe204c209f9b91e4fb247e1205a8
Andrzej Michalski <113372492+Michalski84@users.noreply.github.com>
2023-02-22 21:45:51 +0100

    z9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+
+const btn = document.querySelector(".change-color");
+const bgColor = document.querySelector("body");
+const nameColor = document.querySelector(".color");
+
+btn.addEventListener("click", (event) => {
+  bgColor.style.backgroundColor = getRandomHexColor();
+  nameColor.textContent = getRandomHexColor();
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
